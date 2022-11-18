8c9a392b6456564d6af162616618057309ac2d3f
Pierre Buclez <buclez.p@etoncollege.org.uk>
2022-11-18 16:57:23 +0000

    Initial Commit

---
+++ challenge17swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  challenge17swift
+//
+//  Created by Buclez, Pierre (JD) on 18/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ challenge17swift/challenge17swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  challenge17swiftApp.swift
+//  challenge17swift
+//
+//  Created by Buclez, Pierre (JD) on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct challenge17swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
